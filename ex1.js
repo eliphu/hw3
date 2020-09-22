39b4388df9c51ef0df02fca1d5f287928226d5ff
eliz <elizabeth.phung@student.csulb.edu>
2020-09-22 16:57:27 -0700

    v4

--- ex1.js
+++ ex1.js
@@ -9,9 +9,19 @@ Example 1 JavaScript code
         var student = prompt("Please enter your name","name");
         var grade = prompt("Please enter your grade", "units");
         var text;
-        if (grade == 30) {
-            text = "Hello " + student + " Your grade standing is Freshman"}
+        if (grade >= 30) {
+            text = "Hello " + student + " Your grade standing is Freshman";}
+        else if (grade < 30 && grade >= 60) {
+            text = "Hello " + student + " Your grade standing is Sophmore";
+        }
+        else if (grade < 60 && grade >= 90) {
+            text = "Hello " + student + " Your grade standing is Junior";
+        }
+        else {
+            text = "Hello " + student + " Your grade standing is Senior";
+        }
         document.getElementById("demo").innerHTML = text;
+
     }
     classStanding();
 </script>
